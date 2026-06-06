727b8c80aef98885304199d2eca6be500f94fcca
DragoncyK <kasputis.dragoncycode2007@gmail.com>
2026-06-07 01:25:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   API_BASE: "https://TU_VPS_DOMAIN.com",
   CLIENT_ID: "1314363320974708787",
-  REDIRECT_URI: "https://TU_GITHUB_USERNAME.github.io/kaelion-dashboard/",
+  REDIRECT_URI: "https://dragoncyk.github.io/KaelionDashboard/",
 };
