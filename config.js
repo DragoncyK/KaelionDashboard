5c1bde2d8314b0fd21bca8f101483db4031ffd64
DragoncyK <kasputis.dragoncycode2007@gmail.com>
2026-06-07 01:22:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.KAELION_DASH_CONFIG = {
-  API_BASE_URL: "http://127.0.0.1:8000",
-  DASHBOARD_TITLE: "Kaelion Global Dashboard",
-  BOT_INVITE_URL: "https://discord.com/oauth2/authorize?client_id=123456789012345678&scope=bot%20applications.commands&permissions=8",
+const CONFIG = {
+  API_BASE: "https://TU_VPS_DOMAIN.com",
+  CLIENT_ID: "TU_DISCORD_CLIENT_ID",
+  REDIRECT_URI: "https://TU_GITHUB_USERNAME.github.io/kaelion-dashboard/",
 };
