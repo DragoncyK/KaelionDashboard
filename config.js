ee5accbbeca29557c33e2c7c887664d527c142eb
DragoncyK <kasputis.dragoncycode2007@gmail.com>
2026-06-07 14:03:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const CONFIG = {
-  API_BASE: "http://62.171.155.57:8000",
-  CLIENT_ID: "1314363320974708787",
-  REDIRECT_URI: "https://dragoncyk.github.io/KaelionDashboard/",
+  API_BASE: "https://kaelion.duckdns.org",
+  FRONTEND_URL: "https://dragoncyk.github.io/KaelionDashboard/"
 };
